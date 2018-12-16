51eb728c71fe800a8a76bd96f9662dd8308488e3
DdsT <hfr.ddst@gmail.com>
2018-12-16 21:48:41 +0100

    Update hfrcolortag.meta.js

--- hfrcolortag.meta.js
+++ hfrcolortag.meta.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name        [HFR] Color Tag
-// @namespace   DdsT
-// @version     2.0.1
+// @namespace   ddst.github.io
+// @version     2.1.0
 // @author      DdsT
-// @downloadURL https://github.com/DdsT/hfr_ColorTag/raw/master/hfrcolortag.user.js
-// @updateURL   https://github.com/DdsT/hfr_ColorTag/raw/master/hfrcolortag.meta.js
+// @downloadURL https://ddst.github.io/hfr_ColorTag/hfrcolortag.user.js
+// @updateURL   https://ddst.github.io/hfr_ColorTag/hfrcolortag.meta.js
 // @description Colorier et Annoter les messages en fonction du pseudo
 // @icon        https://www.hardware.fr/images_skin_2010/facebook/logo.png
 // @match       *://forum.hardware.fr/forum2.php*
 // @match       *://forum.hardware.fr/hfr/*/*sujet_*
+// @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand

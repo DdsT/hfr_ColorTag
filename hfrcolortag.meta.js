2b1419eeac18056c97b6ce039a854fe0aedd0419
DdsT <hfr.ddst@gmail.com>
2018-12-19 16:15:59 +0100

    v2.3.0

--- hfrcolortag.meta.js
+++ hfrcolortag.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        [HFR] Color Tag
 // @namespace   ddst.github.io
-// @version     2.2.0
+// @version     2.3.0
 // @author      DdsT
 // @downloadURL https://ddst.github.io/hfr_ColorTag/hfrcolortag.user.js
 // @updateURL   https://ddst.github.io/hfr_ColorTag/hfrcolortag.meta.js
